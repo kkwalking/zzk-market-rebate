1ad178fabeee97ed12358a5fc4900749bc4fae9f
Kelton <417160807@qq.com>
2024-09-24 23:35:37 +0800

    vo2dto补充

--- docs/dev-ops/command-help.md
+++ docs/dev-ops/command-help.md
@@ -1,3 +1,6 @@
 转储sql文件
 
 `mysqldump -P13306 -u root -p -B zzk-market-rebate > zzk-market-rebate.sql`
+
+vo2dto 插件使用
+alt+insert 调出generate面板查找vo2dto
--- zzk-market-rebate-app/src/test/java/top/kelton/test/ApiTest.java
+++ zzk-market-rebate-app/src/test/java/top/kelton/test/ApiTest.java
@@ -1,10 +1,12 @@
 package top.kelton.test;
 
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import top.kelton.infrastructure.persistent.po.UserPO;
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -13,7 +15,17 @@ public class ApiTest {
 
     @Test
     public void test() {
-        log.info("测试完成");
+        UserPO userPO = new UserPO();
+        userPO.setId(1);
+        userPO.setUserId("zzk01");
+        userPO.setUserName("zzk");
+        userPO.setAge(18);
+        log.info("userPO: {}", JSON.toJSONString(userPO));
+        User user = new User();
+        user.setUserName(userPO.getUserName());
+        user.setAge(String.valueOf(userPO.getAge()));
+        log.info("user: {}", JSON.toJSONString(user));
+
     }
 
 }
